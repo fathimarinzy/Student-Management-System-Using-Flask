d13597fb3c90274ef4925db6781668ccc39ce145
fathimarinzy <fathimarinzy02@gmail.com>
2025-01-15 20:19:46 +0530

    static loading

--- student.py
+++ student.py
@@ -9,6 +9,10 @@ app.secret_key='abc'
 def home():
     return render_template("home.html")
 
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
 
 @app.route('/register')
 def register():
